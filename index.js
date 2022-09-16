4169be4430a5cdcfbf6d14efac8558ed54296f57
ANI <aniruddha@squbesoftsol.com>
2022-09-16 17:12:33 +0530

    up

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express')
 var multer  = require('multer')
+const mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+var mongo = require("./models/connection")
+var uschema = require("./models/userData.model")
+
 var port = 3000;
 
 var app = express()
@@ -13,7 +18,6 @@ var storage = multer.diskStorage({
     }
 })
 var upload = multer({ storage: storage })
-
 
 app.use(express.static(__dirname + '/public'));
 app.use('/uploads', express.static('uploads'));
@@ -40,6 +44,61 @@ app.post('/profile-upload-multiple', upload.array('profile-files', 12), function
     
     return res.send(response)
 })
-   
 
-app.listen(port,() => console.log(`Server running on port ${port}!\nClick http://localhost:3000/`))
+
+
+//---------- (get data from HTML to node server) ----------// 
+// app.post("/",upload.none(), (req, res) => {
+//   console.log(req.body)
+// })
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.post("/login.html", (req, res) => {
+  console.log(req.body)
+  //------------------ (mongodb) ------------------//
+  var mongodata = new uschema({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.pass  
+  });
+
+  // mongodata.save();
+
+  mongodata.save(function(err,result){
+    if (err){
+      // console.log(err);
+      console.log("Error occured while saving data!");
+    }
+    else{
+      // console.log(result)
+      console.log("Data saved in database")
+    }
+  })
+  //------------------ (mongodb) ------------------//
+
+  res.send("done!")
+})
+
+//---------- (get data from HTML to node server) ----------//
+
+
+
+
+
+
+
+//------------------ (JsonWebToken) ------------------//
+var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+//------------------ (JsonWebToken) ------------------//
+
+
+
+app.listen(port,() => console.log(`Server running on port ${port}!`))
+
+
+
+
+
